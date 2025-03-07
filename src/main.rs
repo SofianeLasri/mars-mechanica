1026a39c0f7d9906204afd86d253b1d1d5d5be86
SofianeLasri <alasri250@gmail.com>
2025-03-07 11:27:20 +0100

    Add mouse position display and update system for debug text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod plugins;
 mod systems;
 
 use crate::plugins::EntityPlugin;
-use crate::systems::debug_text;
+use crate::systems::{debug_text, update_debug_text};
 use bevy::DefaultPlugins;
 use bevy::color::palettes::basic::PURPLE;
 use bevy::prelude::*;
@@ -13,6 +13,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(EntityPlugin)
         .add_systems(Startup, (setup, debug_text))
+        .add_systems(Update, update_debug_text)
         .run();
 }
 
--- src/systems/debug_text.rs
+++ src/systems/debug_text.rs
@@ -1,14 +1,17 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+pub struct DebugText;
+
 pub fn debug_text(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
     commands.spawn((
-        Text::new("Test"),
+        Text::new("Mouse position: (0.0, 0.0)"),
         TextFont {
             font: asset_server.load("fonts/inter-regular.ttf"),
-            font_size: 50.0,
+            font_size: 18.0,
             ..default()
         },
         Node {
@@ -16,6 +19,25 @@ pub fn debug_text(
             top: Val::Px(5.0),
             left: Val::Px(5.0),
             ..default()
-        }
+        },
+        DebugText,
     ));
+}
+
+pub fn update_debug_text(
+    mut text_query: Query<Entity, With<DebugText>>,
+    window_query: Query<&Window>,
+    mut writer: TextUiWriter,
+) {
+    let window = window_query.single();
+
+    let cursor_position = if let Some(position) = window.cursor_position() {
+        let window_size = Vec2::new(window.width(), window.height());
+        let position = position - window_size / 2.0;
+        position
+    } else {
+        Vec2::new(0.0, 0.0)
+    };
+    
+    *writer.text(text_query.single_mut(), 0) = format!("Mouse position: ({:.1}, {:.1})", cursor_position.x, cursor_position.y);
 }
