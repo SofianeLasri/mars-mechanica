bc705e92a0eb45b05a92a7d8f270716b3380c2a3
SofianeLasri <alasri250@gmail.com>
2025-03-07 10:33:45 +0100

    Add terrain textures

--- src/plugins/entity_plugin.rs
+++ src/plugins/entity_plugin.rs
@@ -5,7 +5,7 @@ pub struct EntityPlugin;
 
 impl Plugin for EntityPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_entity);
+        app.add_systems(Startup, spawn_multiple_squares);
     }
 }
 
@@ -18,6 +18,38 @@ fn spawn_entity(
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(PURPLE))),
-        Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(128.)),
+        Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(64.)),
     ));
+}
+
+fn spawn_multiple_squares(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // Définir les positions où tu veux placer tes carrés
+    let positions = [
+        Vec2::new(100.0, 100.0),
+        Vec2::new(-100.0, 150.0),
+        Vec2::new(200.0, -100.0),
+        Vec2::new(-150.0, -150.0),
+        Vec2::new(0.0, 200.0),
+    ];
+
+    let image = asset_server.load("textures/terrain/inter-corner.png");
+    let material = materials.add(ColorMaterial {
+        texture: Some(image),
+        ..Default::default()
+    });
+
+    // Créer un carré à chaque position
+    for position in positions.iter() {
+        commands.spawn((
+            Mesh2d(meshes.add(Rectangle::default())),
+            MeshMaterial2d(material.clone()),
+            //Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::splat(64.)),
+            Transform::from_xyz(position.x, position.y, 0.0).with_scale(Vec3::splat(64.)),
+        ));
+    }
 }
