0e10cb6ec65c6e98e88430605ffb694c1807937c
SofianeLasri <alasri250@gmail.com>
2025-03-07 13:43:46 +0100

    Refactor camera panning logic and improve pan speed control

--- src/systems/camera.rs
+++ src/systems/camera.rs
@@ -10,6 +10,7 @@ pub struct ControlledCamera {
     pub max_zoom: f32,
     pub pan_speed: f32,
     pub is_panning: bool,
+    pub cursor_start_position: Vec2,
 }
 
 pub fn init_camera(mut commands: Commands) {
@@ -19,8 +20,9 @@ pub fn init_camera(mut commands: Commands) {
             zoom_speed: 0.1,
             min_zoom: 0.1, // Zoom maximum (vue très proche)
             max_zoom: 5.0, // Zoom minimum (vue très éloignée)
-            pan_speed: 0.1,
+            pan_speed: 1.0, // Augmenté pour un meilleur contrôle
             is_panning: false,
+            cursor_start_position: Vec2::new(0.0, 0.0),
         },
     ));
 }
@@ -33,7 +35,6 @@ pub fn update_camera(
     )>,
     mut mouse_button_input_events: EventReader<MouseButtonInput>,
     mut mouse_wheel_events: EventReader<MouseWheel>,
-    mut mouse_motion_events: EventReader<MouseMotion>,
     window_query: Query<&Window>,
 ) {
     let (mut projection, mut transform, mut camera) = match camera_query.get_single_mut() {
@@ -49,55 +50,26 @@ pub fn update_camera(
     }
 
     // Gestion du déplacement avec le clic droit
-    /*for event in mouse_button_input_events.read() {
-        info!("{:?}", event);
-    }*/
-    let mouse_button_input_event = mouse_button_input_events.read().last().cloned();
-    let is_right_mouse_button_pressed = mouse_button_input_event.map_or(false, |event| {
-        event.button == MouseButton::Right && event.state == ButtonState::Pressed
-    });
-
-    let is_right_mouse_button_released = mouse_button_input_event.map_or(false, |event| {
-        event.button == MouseButton::Right && event.state == ButtonState::Released
-    });
-
-    if is_right_mouse_button_pressed {
-        camera.is_panning = true;
-    } else if is_right_mouse_button_released {
-        camera.is_panning = false;
+    for event in mouse_button_input_events.read() {
+        if event.button == MouseButton::Right {
+            camera.is_panning = event.state == ButtonState::Pressed;
+        }
     }
 
+    // Si on est en train de déplacer (clic droit enfoncé)
     if camera.is_panning {
-        for event in mouse_motion_events.read() {
-
-            // We need to get the window size and the current zoom to calculate the pan speed
-            let window = window_query.single();
-            let window_size = Vec2::new(window.width(), window.height());
-
-            // The mouse delta must be normalized and the origin must be at the center of the window
-            let mouse_delta = if let Some(position) = window.cursor_position() {
-                let window_size = Vec2::new(window.width(), window.height());
-                let position = (position - window_size / 2.0) / -2.0;
-                position
-            } else {
-                Vec2::new(0.0, 0.0)
-            };
-
-            info!("{:?}", mouse_delta);
-
-            /*let pan_delta = Vec2::new(
-                -event.delta.x * camera.pan_speed / projection.scale,
-                event.delta.y * camera.pan_speed / projection.scale,
-            );*/
-
-            let pan_delta = Vec2::new(
-                -mouse_delta.x * camera.pan_speed / projection.scale,
-                mouse_delta.y * camera.pan_speed / projection.scale,
-            );
-
-            // Appliquer le déplacement à la caméra
-            transform.translation.x += pan_delta.x;
-            transform.translation.y += pan_delta.y;
+        let window = window_query.single();
+        let window_size = Vec2::new(window.width(), window.height());
+        let cursor_position = window.cursor_position().unwrap() - window_size / 2.0;
+        if camera.cursor_start_position == Vec2::new(0.0, 0.0) {
+            camera.cursor_start_position = cursor_position;
         }
+
+        // Appliquer le déplacement à la caméra
+        let new_position = (cursor_position - camera.cursor_start_position) / projection.scale;
+
+        // Appliquer le déplacement à la caméra
+        transform.translation.x = new_position.x * -1.0;
+        transform.translation.y = new_position.y;
     }
 }
