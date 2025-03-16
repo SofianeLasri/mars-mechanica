bca3f4907db5fb63994a0c3279ae499904a8a3ac
SofianeLasri <alasri250@gmail.com>
2025-03-16 21:42:10 +0100

    Set custom window title for the application; update WindowPlugin configuration

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,19 @@ use crate::plugins::{
     CameraPlugin, DebugTextPlugin, EntityPlugin, InteractionPlugin, TerrainPlugin,
 };
 use crate::systems::generate_world;
-use bevy::prelude::{App, Startup};
-use bevy::DefaultPlugins;
+use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Mars mechanica - Pre pre alpha".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+        )
         .add_plugins((
             EntityPlugin,
             CameraPlugin,
