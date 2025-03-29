7ae2e9a0012611a924fc3a0242a930adf666e5fa
SofianeLasri <alasri250@gmail.com>
2025-03-29 16:37:32 +0100

    feat: update dependencies to version 0.16.0-rc.2 and refactor debug UI initialization

--- src/plugins/debug_ui.rs
+++ src/plugins/debug_ui.rs
@@ -19,7 +19,10 @@ pub struct DebugUiPlugin;
 
 impl Plugin for DebugUiPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::InGame), init)
+        app.add_systems(
+            OnEnter(GameState::InGame),
+            (init_debug_bar, init_debug_toolbox),
+        )
             .add_systems(
                 FixedUpdate,
                 (update_debug_camera_text).run_if(in_state(GameState::InGame)),
@@ -31,7 +34,7 @@ impl Plugin for DebugUiPlugin {
     }
 }
 
-pub fn init(mut commands: Commands, ui_assets: Res<UiAssets>) {
+fn init_debug_bar(mut commands: Commands, ui_assets: Res<UiAssets>) {
     let root_entity = commands
         .spawn((
             Node {
@@ -48,19 +51,176 @@ pub fn init(mut commands: Commands, ui_assets: Res<UiAssets>) {
         ))
         .id();
 
-    commands.entity(root_entity).with_related::<ChildOf>(|child_spawner| {
-        spawn_debug_column(child_spawner, |col_spawner| {
-            spawn_debug_text(col_spawner, &ui_assets, "Mouse position: (0.0, 0.0)", DebugCameraText);
-            spawn_debug_text(col_spawner, &ui_assets, "Hovered cell: None", DebugHoverText);
-        });
+    commands
+        .entity(root_entity)
+        .with_related::<ChildOf>(|child_spawner| {
+            spawn_bar_column(child_spawner, |col_spawner| {
+                spawn_bar_text(
+                    col_spawner,
+                    &ui_assets,
+                    "Mouse position: (0.0, 0.0)",
+                    DebugCameraText,
+                );
+                spawn_bar_text(
+                    col_spawner,
+                    &ui_assets,
+                    "Hovered cell: None",
+                    DebugHoverText,
+                );
+            });
 
-        spawn_debug_column(child_spawner, |col_spawner| {
-            spawn_debug_text(col_spawner, &ui_assets, "FPS: --", FpsCounterText);
+            spawn_bar_column(child_spawner, |col_spawner| {
+                spawn_bar_text(col_spawner, &ui_assets, "FPS: --", FpsCounterText);
+            });
         });
-    });
 }
 
-fn spawn_debug_text<M: Component>(
+fn init_debug_toolbox(mut commands: Commands, ui_assets: Res<UiAssets>) {
+    let toolbox_root = commands
+        .spawn((Node {
+            position_type: PositionType::Absolute,
+            top: Val::Px(16.0),
+            right: Val::Px(16.0),
+            width: Val::Px(255.0),
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },))
+        .id();
+
+    let toolbox_title = (
+        Node {
+            width: Val::Percent(100.0),
+            padding: UiRect {
+                left: Val::Px(8.0),
+                right: Val::Px(8.0),
+                top: Val::Px(4.0),
+                bottom: Val::Px(4.0),
+            },
+            ..default()
+        },
+        BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.9)),
+        children![(
+            Text::new("Debug Toolbox"),
+            TextFont {
+                font: ui_assets.fonts.last().unwrap().clone(),
+                font_size: 14.0,
+                ..default()
+            },
+            TextColor(Color::WHITE),
+        )],
+    );
+
+    let toolbox_content = (
+        Node {
+            width: Val::Percent(100.0),
+            padding: UiRect::all(Val::Px(8.0)),
+            flex_direction: FlexDirection::Column,
+            row_gap: Val::Px(8.0),
+            ..default()
+        },
+        BackgroundColor(Color::srgba(76.0 / 255.0, 76.0 / 255.0, 76.0 / 255.0, 0.9)),
+    );
+
+    commands
+        .entity(toolbox_root)
+        .with_related::<ChildOf>(|child_spawner| {
+            child_spawner.spawn(toolbox_title);
+            child_spawner
+                .spawn(toolbox_content)
+                .with_related::<ChildOf>(|content_spawner| {
+                    // Section
+                    content_spawner.spawn((
+                        Node {
+                            flex_direction: FlexDirection::Column,
+                            row_gap: Val::Px(4.0),
+                            ..default()
+                        },
+                        children![
+                            // Section title
+                            (
+                                Text::new("Cell Mouse Selection"),
+                                TextFont {
+                                    font: ui_assets.fonts.last().unwrap().clone(),
+                                    font_size: 14.0,
+                                    ..default()
+                                },
+                                TextColor(Color::WHITE),
+                            ),
+                            // Section content
+                            (
+                                Node {
+                                    flex_direction: FlexDirection::Column,
+                                    row_gap: Val::Px(4.0),
+                                    ..default()
+                                },
+                                children![
+                                    (
+                                        // Property
+                                        Node {
+                                            flex_direction: FlexDirection::Row,
+                                            column_gap: Val::Px(4.0),
+                                            ..default()
+                                        },
+                                        children![
+                                            (
+                                                // Checked checkbox
+                                                Node {
+                                                    width: Val::Px(16.0),
+                                                    height: Val::Px(16.0),
+                                                    ..default()
+                                                },
+                                                BackgroundColor(Color::srgba(0.0, 1.0, 0.0, 1.0)),
+                                            ),
+                                            (
+                                                // Label
+                                                Text::new("Solid Objects"),
+                                                TextFont {
+                                                    font: ui_assets.fonts.last().unwrap().clone(),
+                                                    font_size: 14.0,
+                                                    ..default()
+                                                },
+                                                TextColor(Color::WHITE),
+                                            )
+                                        ],
+                                    ),
+                                    (
+                                        // Property
+                                        Node {
+                                            flex_direction: FlexDirection::Row,
+                                            column_gap: Val::Px(4.0),
+                                            ..default()
+                                        },
+                                        children![
+                                            (
+                                                // Unchecked checkbox
+                                                Node {
+                                                    width: Val::Px(16.0),
+                                                    height: Val::Px(16.0),
+                                                    ..default()
+                                                },
+                                                BackgroundColor(Color::srgba(1.0, 0.0, 0.0, 1.0)),
+                                            ),
+                                            (
+                                                // Label
+                                                Text::new("Entities"),
+                                                TextFont {
+                                                    font: ui_assets.fonts.last().unwrap().clone(),
+                                                    font_size: 14.0,
+                                                    ..default()
+                                                },
+                                                TextColor(Color::WHITE),
+                                            )
+                                        ],
+                                    )
+                                ],
+                            )
+                        ],
+                    ));
+                });
+        });
+}
+
+fn spawn_bar_text<M: Component>(
     spawner: &mut RelatedSpawnerCommands<ChildOf>,
     ui_assets: &UiAssets,
     text_content: &str,
@@ -78,18 +238,16 @@ fn spawn_debug_text<M: Component>(
     ));
 }
 
-fn spawn_debug_column(
+fn spawn_bar_column(
     spawner: &mut RelatedSpawnerCommands<ChildOf>,
     spawn_contents: impl FnOnce(&mut RelatedSpawnerCommands<ChildOf>),
 ) {
     spawner
-        .spawn((
-            Node {
-                flex_direction: FlexDirection::Column,
-                row_gap: Val::Px(4.0),
-                ..default()
-            },
-        ))
+        .spawn((Node {
+            flex_direction: FlexDirection::Column,
+            row_gap: Val::Px(4.0),
+            ..default()
+        },))
         .with_related::<ChildOf>(|col_spawner| {
             spawn_contents(col_spawner);
         });
@@ -115,7 +273,6 @@ pub fn update_debug_camera_text(
         cursor_position.x, cursor_position.y
     );
 }
-
 
 fn update_fps_counter(
     text_query: Query<Entity, With<FpsCounterText>>,
