3a1e1a89926c32d7b843c8a5e23b28d37abdfcf2
SofianeLasri <alasri250@gmail.com>
2025-03-29 10:42:51 +0100

    feat: enhance debug UI with FPS counter and refactor text spawning logic

--- src/plugins/debug_ui.rs
+++ src/plugins/debug_ui.rs
@@ -1,14 +1,19 @@
-use crate::components::UiAssets;
-use crate::GameState;
 use bevy::color::palettes::css::BLACK;
+use bevy::ecs::relationship::RelatedSpawnerCommands;
 use bevy::prelude::*;
 use bevy::ui::{FlexDirection, UiRect};
+
+use crate::components::UiAssets;
+use crate::GameState;
 
 #[derive(Component)]
 pub struct DebugCameraText;
 
 #[derive(Component)]
 pub struct DebugHoverText;
+
+#[derive(Component)]
+struct FpsCounterText;
 
 pub struct DebugUiPlugin;
 
@@ -21,8 +26,9 @@ impl Plugin for DebugUiPlugin {
             );
     }
 }
+
 pub fn init(mut commands: Commands, ui_assets: Res<UiAssets>) {
-    commands
+    let root_entity = commands
         .spawn((
             Node {
                 position_type: PositionType::Absolute,
@@ -30,42 +36,58 @@ pub fn init(mut commands: Commands, ui_assets: Res<UiAssets>) {
                 left: Val::Px(0.0),
                 width: Val::Percent(100.0),
                 flex_direction: FlexDirection::Row,
+                column_gap: Val::Px(4.0),
                 padding: UiRect::all(Val::Px(8.0)),
                 ..default()
             },
             BackgroundColor::from(BLACK),
         ))
-        .with_children(|parent| {
-            parent
-                .spawn((
-                    Node {
-                        flex_direction: FlexDirection::Column,
-                        row_gap: Val::Px(4.0),
-                        ..default()
-                    },
-                ))
-                .with_children(|parent| {
-                    parent.spawn((
-                        Text::new("Mouse position: (0.0, 0.0)"),
-                        TextFont {
-                            font: ui_assets.fonts.first().unwrap().clone(),
-                            font_size: 14.0,
-                            ..default()
-                        },
-                        TextColor(Color::WHITE),
-                        DebugCameraText,
-                    ));
-                    parent.spawn((
-                        Text::new("Hovered cell: None"),
-                        TextFont {
-                            font: ui_assets.fonts.first().unwrap().clone(),
-                            font_size: 14.0,
-                            ..default()
-                        },
-                        TextColor(Color::WHITE),
-                        DebugHoverText,
-                    ));
-                });
+        .id();
+
+    commands.entity(root_entity).with_related::<ChildOf>(|child_spawner| {
+        spawn_debug_column(child_spawner, |col_spawner| {
+            spawn_debug_text(col_spawner, &ui_assets, "Mouse position: (0.0, 0.0)", DebugCameraText);
+            spawn_debug_text(col_spawner, &ui_assets, "Hovered cell: None", DebugHoverText);
+        });
+
+        spawn_debug_column(child_spawner, |col_spawner| {
+            spawn_debug_text(col_spawner, &ui_assets, "FPS: --", FpsCounterText);
+        });
+    });
+}
+
+fn spawn_debug_text<M: Component>(
+    spawner: &mut RelatedSpawnerCommands<ChildOf>,
+    ui_assets: &UiAssets,
+    text_content: &str,
+    marker: M,
+) {
+    spawner.spawn((
+        Text::new(text_content),
+        TextFont {
+            font: ui_assets.fonts.first().unwrap().clone(),
+            font_size: 14.0,
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        marker,
+    ));
+}
+
+fn spawn_debug_column(
+    spawner: &mut RelatedSpawnerCommands<ChildOf>,
+    spawn_contents: impl FnOnce(&mut RelatedSpawnerCommands<ChildOf>),
+) {
+    spawner
+        .spawn((
+            Node {
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Px(4.0),
+                ..default()
+            },
+        ))
+        .with_related::<ChildOf>(|col_spawner| {
+            spawn_contents(col_spawner);
         });
 }
 
@@ -78,10 +100,9 @@ pub fn update_debug_camera_text(
 
     let cursor_position = if let Some(position) = window.cursor_position() {
         let window_size = Vec2::new(window.width(), window.height());
-        let position = position - window_size / 2.0;
-        position
+        position - window_size / 2.0
     } else {
-        Vec2::new(0.0, 0.0)
+        Vec2::ZERO
     };
 
     let text_entity = text_query.single().unwrap();
