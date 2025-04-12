f353a31ec5f46b250daebfc2bbb19268073fb367
SofianeLasri <alasri250@gmail.com>
2025-04-12 17:57:45 +0200

    feat(window): add window icon from asset and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@ use crate::plugins::asset_preloader::AssetPreloaderPlugin;
 use crate::plugins::game::GamePlugin;
 use crate::plugins::splash::SplashPlugin;
 use crate::plugins::ui::UiPlugin;
-use bevy::prelude::{App, AppExtStates, ClearColor, Color, PluginGroup, Resource, States, Window, WindowPlugin};
+use bevy::prelude::{App, AppExtStates, ClearColor, Color, Entity, NonSend, PluginGroup, Query, Resource, Startup, States, Window, WindowPlugin, With};
+use bevy::window::PrimaryWindow;
+use bevy::winit::WinitWindows;
 use bevy::DefaultPlugins;
+use ::image::open;
+use winit::window::Icon;
 
 #[derive(Resource)]
 struct CliArgs {
@@ -30,17 +34,37 @@ fn main() {
 
     App::new()
         .insert_resource(CliArgs { skip_splash })
-        .add_plugins(DefaultPlugins
-            .set(WindowPlugin {
-                primary_window: Some(Window {
-                    title: "Mars mechanica - Pre pre alpha".to_string(),
-                    ..Default::default()
-                }),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Mars mechanica - Pre pre alpha".to_string(),
                 ..Default::default()
-            })
-        )
+            }),
+            ..Default::default()
+        }))
         .insert_resource(ClearColor(Color::BLACK))
         .init_state::<GameState>()
         .add_plugins((AssetPreloaderPlugin, SplashPlugin, UiPlugin, GamePlugin))
+        .add_systems(Startup, set_window_icon)
         .run();
+}
+
+fn set_window_icon(
+    windows: NonSend<WinitWindows>,
+    primary_window_query: Query<Entity, With<PrimaryWindow>>,
+) {
+    let primary_window_entity = primary_window_query.single().unwrap();
+    let primary_window = windows.get_window(primary_window_entity).unwrap();
+
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = open("assets/textures/ui/logo.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+
+    primary_window.set_window_icon(Some(icon));
 }
